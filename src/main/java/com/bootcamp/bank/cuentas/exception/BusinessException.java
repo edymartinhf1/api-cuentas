0f565c471be38db9ec1b4635783aec1aaae695d2
edymartinhf1 <edymartinhf1@gmail.com>
2023-07-17 19:47:54 -0500

    sonar fix

--- src/main/java/com/bootcamp/bank/cuentas/exception/BusinessException.java
+++ src/main/java/com/bootcamp/bank/cuentas/exception/BusinessException.java
@@ -4,12 +4,9 @@ package com.bootcamp.bank.cuentas.exception;
  * Clase Exception de Negocio
  */
 public class BusinessException extends RuntimeException{
-    private String errorCode;
-    private String messageError;
 
     public BusinessException(String messageError) {
         super(messageError);
-        this.messageError = messageError;
     }
 
 
--- src/test/java/com/bootcamp/bank/ApiCuentasApplicationTests.java
+++ src/test/java/com/bootcamp/bank/ApiCuentasApplicationTests.java
@@ -1,9 +1,15 @@
 package com.bootcamp.bank;
 
+import com.bootcamp.bank.cuentas.controller.CuentasController;
+import com.bootcamp.bank.cuentas.model.dao.CuentaDao;
 import com.bootcamp.bank.cuentas.service.CuentaServiceI;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -11,9 +17,18 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 class ApiCuentasApplicationTests {
 	@Autowired
 	private CuentaServiceI cuentaServiceI;
+
 	@Test
 	void contextLoads() {
 		assertThat(cuentaServiceI).isNotNull();
+	}
+
+	@Test
+	void findAllAccount() {
+		// When
+		List<CuentaDao> cuentas = this.cuentaServiceI.findAll().collectList().block();
+		// Then
+		assertThat(cuentas.size()>0);
 	}
 
 }
