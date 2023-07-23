5b1e4235fdbce064d93b1255bd8f77688bb5752d
edymartinhf1 <edymartinhf1@gmail.com>
2023-07-22 22:45:36 -0500

    update

--- src/main/java/com/bootcamp/bank/cuentas/strategy/cliente/PerfilPersonalSstrategy.java
+++ src/main/java/com/bootcamp/bank/cuentas/strategy/cliente/PerfilPersonalSstrategy.java
@@ -2,9 +2,10 @@ package com.bootcamp.bank.cuentas.strategy.cliente;
 
 import com.bootcamp.bank.cuentas.model.Cliente;
 import com.bootcamp.bank.cuentas.model.PerfilInfo;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
-
+@Component
 public class PerfilPersonalSstrategy implements PerfilClienteStrategy {
     @Override
     public PerfilInfo configurarPerfil(Cliente cliente) {
