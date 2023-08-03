b3f36737001868b97ca52368cbe090c1c0dc55dd
edymartinhf1 <edymartinhf1@gmail.com>
2023-08-02 21:30:19 -0500

    update

--- src/main/java/com/bootcamp/bank/cuentas/model/Cuenta.java
+++ src/main/java/com/bootcamp/bank/cuentas/model/Cuenta.java
@@ -1,5 +1,9 @@
 package com.bootcamp.bank.cuentas.model;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
@@ -12,6 +16,8 @@ public class Cuenta {
     private String  idCliente;
     private String  numeroCuenta;
     private String fechaCreacionT;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime fechaCreacion;
     private String  estado;
     private String  tipoCuenta; // AHO: ahorro  , CTE : cuenta corriente , PZF: plazo fijo
